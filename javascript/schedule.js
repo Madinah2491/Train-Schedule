0fa264623f5a690961e3c39264491bf765cac4e9
Madinah2491 <madinah2491@gmail.com>
2018-03-24 17:49:37 -0400

    added firebase

--- javascript/schedule.js
+++ javascript/schedule.js
@@ -1,3 +1,14 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAxrxNLTvPc9brnANMrKOLu5Sv3_C5GXrc",
+    authDomain: "train-schedule-6abc2.firebaseapp.com",
+    databaseURL: "https://train-schedule-6abc2.firebaseio.com",
+    projectId: "train-schedule-6abc2",
+    storageBucket: "train-schedule-6abc2.appspot.com",
+    messagingSenderId: "413289603311"
+  };
+  firebase.initializeApp(config);
+  
 $(document).ready({
 
 });
